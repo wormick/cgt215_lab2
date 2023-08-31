e14aeed94af654044621ba90428d6d92bc02011b
Derick Zhen <dzhen@purdue.edu>
2023-08-31 17:46:44 -0400

    Add project files.

---
+++ cgt215_lab2/cgt215_lab2.cpp
@@ -0,0 +1,18 @@
+// cgt215_lab2.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	float A;
+	float B;
+	float X;
+	std::cout << "its math time\n";
+	cout << "Please enter a value for A: ";
+	cin >> A;
+	cout << "A=" << A << endl;
+	B = (A * 4) + 1;
+	cout << "And B=" << B << endl;
+}
