26edae01e7460d78c523e98aff119233e523bbbd
Derick Zhen <dzhen@purdue.edu>
2023-09-05 15:50:58 -0400

    lab2-done

--- cgt215_lab2/cgt215_lab2.cpp
+++ cgt215_lab2/cgt215_lab2.cpp
@@ -10,9 +10,17 @@ int main()
 	float B;
 	float X;
 	std::cout << "its math time!!\n";
+	std::cout << "today im going to be solving for x of A(x) + B = 0\n";
+
+
 	cout << "Please enter a value for A: ";
 	cin >> A;
 	cout << "A=" << A << endl;
-	B = (A * 4) + 1;
-	cout << "And B=" << B << endl;
+
+	cout << "Please enter a value for B: ";
+	cin >> B;
+	cout << "B=" << B << endl;
+
+	X = (-B / A);
+	cout << "And X=" << X << endl;
 }
